ee2a4d3e62149651e532bad6e166c544e03976c5
Lucas Rosa <lucassantosrosa51@gmail.com>
2021-03-15 18:38:23 -0300

    Componentize Content

--- src/components/Content.tsx
+++ src/components/Content.tsx
@@ -1,3 +1,32 @@
-export function Content() {
-  // Complete aqui
+import { MovieCard } from "./MovieCard";
+
+interface MovieProps {
+  Title: string;
+  Poster: string;
+  Ratings: Array<{
+    Source: string;
+    Value: string;
+  }>;
+  Runtime: string;
+}
+
+interface ContentProps {
+  movies: Array<MovieProps>;
+}
+
+export function Content({ movies }: ContentProps) {
+  return (
+    <main>
+      <div className="movies-list">
+        {movies.map((movie) => (
+          <MovieCard
+            title={movie.Title}
+            poster={movie.Poster}
+            runtime={movie.Runtime}
+            rating={movie.Ratings[0].Value}
+          />
+        ))}
+      </div>
+    </main>
+  );
 }
